58486cef4797f8dacbedface0e34ae9574e7dbae
Sauzlo <saul.mackay@googlemail.com>
2023-06-24 14:41:53 +0100

    classes and tables set up

---
+++ db/travel_bucket_list.sql
@@ -0,0 +1,14 @@
+DROP TABLE cities;
+DROP TABLE countries;
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    rating INT
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE
+);
---
+++ models/country.py
@@ -0,0 +1,5 @@
+class Country:
+    def __init__(self, name, rating, id):
+        self.name = name
+        self.rating = rating
+        self.id = id
