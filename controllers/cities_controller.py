14ef681e83e34b573ccca82dfa844b94bb0c4117
Sauzlo <saul.mackay@googlemail.com>
2023-06-27 16:20:09 +0100

    mvp completed and some css put in place

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -28,3 +28,28 @@ def create():
     city = City(name, country)
     city_repository.save(city)
     return redirect(f'/countries/{country_id}')
+
+
+@cities_blueprint.route('/cities/<id>/edit')
+def edit(id):
+    city = city_repository.select(id)
+    countries = country_repository.select_all()
+    return render_template('cities/edit.html', city=city, countries=countries)
+
+
+@cities_blueprint.route('/cities/<id>', methods=['POST'])
+def update(id):
+    name = request.form["name"]
+    rating = request.form["rating"]
+    visited = "visited" in request.form
+    country_id = request.form['country_id']
+    country = country_repository.select(country_id)
+    city = City(name, country, rating, visited, id)
+    city_repository.update(city)
+    return redirect(f'/cities/{id}')
+
+
+@cities_blueprint.route('/cities/<id>/delete', methods=['POST'])
+def delete(id):
+    city_repository.delete(id)
+    return redirect('/countries')
